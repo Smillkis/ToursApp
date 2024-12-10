3d922406063f3784730168d8df04d5736abeece8
Smillkis <Smillkis@DESKTOP-U6VI7KT>
2024-12-10 18:25:39 +0800

    Add project files.

---
+++ ToursApp/AddEditPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToursApp
+{
+    /// <summary>
+    /// Interaction logic for AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Hotel _currentHotel = new Hotel();
+        public AddEditPage(Hotel selectedHotel)
+        {
+            InitializeComponent();
+
+            if (selectedHotel != null)
+                _currentHotel = selectedHotel;
+            
+            DataContext = _currentHotel;
+            ComboCountries.ItemsSource = ToursBaseEntities.GetContext().Country.ToList();
+        }
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentHotel.Name))
+                errors.AppendLine("Укажите название отеля");
+            if (_currentHotel.CountOfStarts < 1 || _currentHotel.CountOfStarts > 5)
+                errors.AppendLine("Количество звёзд - число от 1 до 5");
+            if (_currentHotel.Country == null)
+                errors.AppendLine("Выберите страну");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (_currentHotel.Id == 0)
+                ToursBaseEntities.GetContext().Hotel.Add(_currentHotel);
+            try
+            {
+                ToursBaseEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация обновлена!");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
